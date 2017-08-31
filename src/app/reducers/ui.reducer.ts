f9b45f31805d96627d3af27f102824c8458b37b4
colinjlacy <colin@webcake.co>
2017-08-31 10:36:16 -0500

    adds comments

--- src/app/reducers/ui.reducer.ts
+++ src/app/reducers/ui.reducer.ts
@@ -1,7 +1,5 @@
 import { Action } from '@ngrx/store';
 
-// this is a reducer function in its most bare-bones form - nothing at all.
-// in the next step we'll start populating this with functionality.
 export function ui(state: any, action: Action) {
 
 }

3b773f599f9a84ebad4db2fc8053356b8ac25fbb
colinjlacy <colin@webcake.co>
2017-08-31 05:42:23 -0500

    setup complete, now to begin programming

--- src/app/views/main/main.view.ts
+++ src/app/views/main/main.view.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'main-view',
-    templateUrl: './main.view.html'
+    templateUrl: './main.view.html',
+    styleUrls: ['./main.view.scss']
 })
 export class MainView {
 

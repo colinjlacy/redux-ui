1c2ed581a4e135dd9cf7f51399982a6e99761808
colinjlacy <colin@webcake.co>
2017-08-31 10:00:48 -0500

    adds comments

--- src/app/components/app-nav/app-nav.component.ts
+++ src/app/components/app-nav/app-nav.component.ts
@@ -12,14 +12,8 @@ export class AppNav {
     navItems: Array<iNavItem>;
 
     @Input() isOpen: boolean;
-    // notice we take the activeNavItem as an input instead of trying
-    // to handle it internally.  That's part of the redux mentality,
-    // and in following that we keep this component stupid
     @Input() activeNavItem: iNavItem;
     @Output() closeNavEvent = new EventEmitter<any>();
-    // this adds an even that we emit once a user has chosen a nav item.
-    // it's up the view to decide what to do with that event, not this
-    // component.
     @Output() setNavItem = new EventEmitter<iNavItem>();
 
     constructor() {

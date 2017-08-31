7030e221a9ddeb10ee4cd1475cde149cd96eb788
colinjlacy <colin@webcake.co>
2017-08-31 09:59:30 -0500

    adds comments

--- src/app/components/page-title/page-title.component.ts
+++ src/app/components/page-title/page-title.component.ts
@@ -6,5 +6,7 @@ import { Component, Input } from '@angular/core';
     styleUrls: ['./page-title.component.scss']
 })
 export class PageTitle {
+    // now I'm setting the page title dynamically, using
+    // data that was passed in from the main view
     @Input() title: string;
 }
